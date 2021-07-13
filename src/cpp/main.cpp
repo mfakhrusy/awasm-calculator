8adf6872f96c49646025494294073504a8737d46
Muhamad Fakhrusy <fakhrusy.m@gmail.com>
2021-07-13 19:32:31 +0700

    add operator method & clear screen method

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -8,11 +8,51 @@ void add_number(std::string n) {
   math_expression += n;
 };
 
+namespace OperatorType {
+  enum Value {
+    add = 1,
+    subtract = 2,
+    multiply = 3,
+    divide = 4,
+    no_op = 5,
+  };
+
+  Value from_string_to_operator (std::string s_operator) {
+
+    if (s_operator == std::string(1, '+')) {
+      return add;
+    } else if (s_operator == std::string(1, '-')) {
+      return subtract;
+    } else if (s_operator == std::string(1, 'x')) {
+      return multiply;
+    } else if (s_operator == std::string(1, '/')) {
+      return divide;
+    } else {
+      return no_op;
+    }
+  }
+};
+
+void add_operator(std::string new_operator) {
+  const std::string last_expression_string = std::string(1, math_expression[math_expression.length() - 1]);
+  const OperatorType::Value last_expression = OperatorType::from_string_to_operator(last_expression_string);
+
+  if (last_expression == OperatorType::no_op) {
+    math_expression += new_operator;
+  }
+}
+
+void clear_math_expression() {
+  math_expression = "";
+}
+
 std::string get_math_expression() {
-  return math_expression;
+    return math_expression;
 };
 
 EMSCRIPTEN_BINDINGS(my_module) {
-  emscripten::function("add_number", &add_number);
   emscripten::function("get_math_expression", &get_math_expression);
+  emscripten::function("add_number", &add_number);
+  emscripten::function("add_operator", &add_operator);
+  emscripten::function("clear_math_expression", &clear_math_expression);
 };
--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@ const initWasm = () => {
       Module.onRuntimeInitialized = async _ => {
         const api = {
           addNumber: Module['add_number'],
+          addOperator: Module['add_operator'],
+          clearMathExpression: Module['clear_math_expression'],
           getMathExpression: Module['get_math_expression'],
         };
 
         resolve({
           addNumber: api.addNumber,
+          addOperator: api.addOperator,
+          clearMathExpression: api.clearMathExpression,
           getMathExpression: api.getMathExpression,
         });
       }
     })
   )
 }
+
+const getTextContent = str => str.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
 
 const main = async () => {
   const api = await initWasm();
@@ -25,16 +31,50 @@ const main = async () => {
   // get calculator screen current text
   const calcScreenText = document.getElementById('calculator__screen-current-text');
 
-  const onClick = (e) => {
-    const numberContent = e.target.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim()
-    api.addNumber(numberContent);
-    const mathExpression = api.getMathExpression();
-    calcScreenText.innerText = mathExpression;
+  const setExpression = () => {
+    calcScreenText.innerText = api.getMathExpression();
+  };
+
+  // define function for number buttons
+  const onClickNumberButton = (e) => {
+    const content = getTextContent(e.target.textContent)
+    api.addNumber(content);
+
+    setExpression();
   };
 
   for (let i = 0; i < numberButtons.length; i += 1) {
-    numberButtons[i].addEventListener('click', onClick)
+    numberButtons[i].addEventListener('click', onClickNumberButton)
   }
+
+  const divisionNode = document.getElementById('calculator__division');
+  const multiplicationNode = document.getElementById('calculator__multiplication');
+  const additionNode = document.getElementById('calculator__addition');
+  const subtractionNode = document.getElementById('calculator__subtraction');
+
+  // define function for 4 operator above
+  const onClickOperatorButton = (e) => {
+    const content = getTextContent(e.target.textContent)
+    api.addOperator(content);
+
+    setExpression()
+  };
+
+  divisionNode.addEventListener('click', onClickOperatorButton);
+  multiplicationNode.addEventListener('click', onClickOperatorButton);
+  additionNode.addEventListener('click', onClickOperatorButton);
+  subtractionNode.addEventListener('click', onClickOperatorButton);
+
+  // AC function (clear screen)
+  const onClickClearButton = () => {
+    api.clearMathExpression();
+
+    setExpression();
+  }
+
+  const clearNode = document.getElementById('calculator__ac');
+  clearNode.addEventListener('click', onClickClearButton);
+
 };
 
 main();
